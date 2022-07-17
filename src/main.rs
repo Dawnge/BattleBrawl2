8a0c96adf1c0f55861ea6da80190ad04125d28e8
BigUff <jevo@Taube.localdomain>
2022-07-17 14:16:36 +0200

    main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{time::{SystemTime, Duration}, thread::sleep};
+
+
+
+fn processInput(){
+
+}
+fn update(){
+
+}
+fn render(){
+
+}
 fn main() {
-    println!("NOT HELLO, mihuahauhauhauahua");
+    let frames = Duration::new(0,5000000);
+    let gameover = true;
+    while gameover {
+        let start = SystemTime::now();
+        processInput();
+        update();
+        render();
+        let dur = match start.elapsed() {
+            Ok(dur) => dur,
+            Err(e) => panic!("{}", e)
+        };
+        sleep(dur+frames);
+    }
 }
