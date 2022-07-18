8df704cf3a4442cd3cba98756fd7da11c0fff679
BigUff <jevo@Taube.localdomain>
2022-07-18 21:23:52 +0200

    gravity applied to player

--- src/config.rs
+++ src/config.rs
@@ -2,32 +2,44 @@ use macroquad::prelude::*;
 
 /// Ich will das in der Documentation sehen!
 pub struct PlayerConfig {
-    pub speed: f32,
+    pub base_speed: f32,
     pub width: f32,
     pub height: f32,
     pub start_pos: (f32,f32),
+    pub base_jump: f32,
+    pub dash: f32,
 }
 pub const PLAYER: PlayerConfig = PlayerConfig {
-    speed: 10.0f32,
+    base_speed: 500.,
     width: 15.,
     height: 15.,
     start_pos: (0.5f32, 0.5f32),
+    base_jump: 30.,
+    dash: 5.,
 };
 impl PlayerConfig {
     pub fn get_start_pos(&self) -> Box<(f32, f32)> {
         Box::new((self.start_pos.0 * screen_width(), self.start_pos.1 * screen_height()))
     }
+    pub fn get_player_speed(&self) -> f32 {
+        self.base_speed / get_fps() as f32
+    }
 }
 
 pub struct ArenaConfig {
     pub floor_height: f32,
+    pub gravity: f32,
 }
 pub const ARENA: ArenaConfig = ArenaConfig {
     floor_height: 0.8,
+    gravity: 1.,
 };
 impl ArenaConfig {
     pub fn get_floor_height(&self) -> f32 {
         self.floor_height * screen_height()
+    }
+    pub fn player_grounded(&self, player_pos: &(f32,f32)) -> bool {
+        player_pos.1 >= self.get_floor_height()-PLAYER.height
     }
 }
 
@@ -41,3 +53,4 @@ pub fn window_conf() -> Conf {
         ..Default::default()
     }
 }
+
--- src/main.rs
+++ src/main.rs
@@ -6,38 +6,56 @@ pub mod config;
 use config::*;
 
 
-fn handle_input(mut player_pos: &mut Box<(f32,f32)>) {
+
+fn handle_input(player_pos: &mut Box<(f32,f32)>, player_vel: &mut Box<(f32,f32)>) {
     if is_key_down(KeyCode::Right) && player_pos.0 < screen_width() - PLAYER.width {
-        player_pos.0 += PLAYER.speed;
+        player_pos.0 += PLAYER.get_player_speed();
     }
     if is_key_down(KeyCode::Left) && player_pos.0 > 0. + PLAYER.width {
         // todo move left event
-        player_pos.0 -= PLAYER.speed;
+        player_pos.0 -= PLAYER.get_player_speed();
     }
     if is_key_down(KeyCode::Down) && player_pos.1 < ARENA.get_floor_height() - PLAYER.height + 75.{
         // todo: dash down event
-        player_pos.1 += PLAYER.speed;
+        player_vel.1 += PLAYER.dash;
     }
-    if (is_key_down(KeyCode::Up) || is_key_down(KeyCode::Space)) && player_pos.1 > 0. + PLAYER.height{
+    if (is_key_down(KeyCode::Up) || is_key_down(KeyCode::Space)) && player_pos.1 > 0. + PLAYER.height && ARENA.player_grounded(player_pos){
         // todo: jump event
-        player_pos.1 -= PLAYER.speed;
+        player_vel.1 -= PLAYER.base_jump;
+    }
+}
+
+fn handle_gravity(player_vel: &mut Box<(f32,f32)>) {
+    player_vel.1+= ARENA.gravity;
+}
+
+fn apply_velocity(player_vel: &mut Box<(f32,f32)>, player_pos: &mut Box<(f32,f32)>) {
+    player_pos.1 += player_vel.1;
+    player_pos.0 += player_vel.0;
+    if ARENA.player_grounded(player_pos) {
+        player_pos.1 = ARENA.get_floor_height() - PLAYER.height;
+        player_vel.1 = 0.;
     }
 }
 #[macroquad::main(window_conf, "BasicShapes")]
 async fn main() {
         // Gamestate
     let mut player_pos: Box<(f32, f32)> = PLAYER.get_start_pos();
-
+    let mut player_vel: Box<(f32, f32)> = Box::new((0.,0.));
 
     loop {
         // create background
         clear_background(DARKGRAY);
         draw_line(0., ARENA.get_floor_height(), screen_width(), ARENA.get_floor_height(), 5., BLACK);
 
-        // handle input
+        // handle/process input
 
-        handle_input(&mut player_pos);
+        handle_input(&mut player_pos, &mut player_vel);
+        apply_velocity(&mut player_vel, &mut player_pos);
 
+        //update
+        handle_gravity(&mut player_vel);
+        
         // TODO
         // move_player(player_pos, player_movement);
         // do_player_physics()
